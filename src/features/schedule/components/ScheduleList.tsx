620463fe281652fc52e39f77ab03d5e7ad510181
minegishik <minegishik@analix.co.jp>
2025-12-16 00:44:10 +0900

    再レンダリングの実装

--- src/features/schedule/components/ScheduleList.tsx
+++ src/features/schedule/components/ScheduleList.tsx
@@ -1,19 +1,49 @@
 import { useScheduleStore} from '../store'
 
-export const ScheduleList = () => {
-    const schedules = useScheduleStore((state) => state.schedules)
+export const ScheduleList = () => { 
 
-    if (schedules.length === 0) {
-        return <p>まだ予定はありません</p>
+    const { schedules, addSchedule } = useScheduleStore()
+
+    const handleAdd = () => {
+        addSchedule({
+            id: crypto.randomUUID(),
+            title: 'ミルク',
+            date: '2025-03-21',
+            memo: '120ml',
+        })
     }
 
-    return (
-        <ul>
-            {schedules.map((s) => (
-                <li key={s.id}>
-                    <strong>{s.title}</strong> ({s.date})
-                </li>
-            ))}
-        </ul>
+    return(
+        <div>
+            <button onClick={handleAdd}>予定を追加</button>
+
+            {schedules.length === 0 ? (
+                <p>まだ予定はありません</p>
+            ) : (
+                <ul>
+                    {schedules.map((s) => (
+                        <li key={s.id}>
+                            {s.date} / {s.title}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+
     )
+    // const schedules = useScheduleStore((state) => state.schedules)
+
+    // if (schedules.length === 0) {
+    //     return <p>まだ予定はありません</p>
+    // }
+
+    // return (
+    //     <ul>
+    //         {schedules.map((s) => (
+    //             <li key={s.id}>
+    //                 <strong>{s.title}</strong> ({s.date})
+    //             </li>
+    //         ))}
+    //     </ul>
+    // )
 }
